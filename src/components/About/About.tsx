10e4df734b488eefa576cece3c87d46a3a1230b2
comzee <samjesberg@gmail.com>
2024-02-19 15:14:38 -0600

    fix: updating styling

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import { Container } from "./styles";
-import VinayakSingh from "../../assets/VinayakSingh.png";
+import SamJesberg from "../../assets/SamJesberg.png";
 import wordpress from "../../assets/wordpress.svg";
 import shopify from "../../assets/shopify.svg";
 import htmlIcon from "../../assets/html-icon.svg";
@@ -11,7 +11,7 @@ import typescriptIcon from "../../assets/typescript-icon.svg";
 import vueIcon from "../../assets/vue-icon.svg";
 import boostrapIcon from "../../assets/bootstrap-icon.svg";
 import ScrollAnimation from "react-animate-on-scroll";
-
+import IllustrationTwo from "../../assets/illustration.png";
 
 export function About() {
   return (
@@ -22,25 +22,38 @@ export function About() {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
           <p>
-          Hi there! I'm Vinayak, a website developer with a passion for creating custom online experiences for my clients. With a skill set including HTML, CSS, JavaScript, and React, I have the tools to bring any website vision to life.
+            Hi there! I'm Vinayak, a website developer with a passion for
+            creating custom online experiences for my clients. With a skill set
+            including HTML, CSS, JavaScript, and React, I have the tools to
+            bring any website vision to life.
           </p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+        <ScrollAnimation
+          animateIn="fadeInLeft"
+          delay={0.2 * 1000}
+          style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        >
           <p>
-          But my services go beyond just custom development - I'm also proficient in using CMS systems like WordPress and Shopify, making it easy for my clients to take control of their own websites and keep them up to date.</p>
+            But my services go beyond just custom development - I'm also
+            proficient in using CMS systems like WordPress and Shopify, making
+            it easy for my clients to take control of their own websites and
+            keep them up to date.
+          </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
           <p>
-          So if you're in need of a new website or just looking to revamp your current online presence, I'd love to chat and see how I can help. Let's bring your website dreams to reality together!</p>
+            So if you're in need of a new website or just looking to revamp your
+            current online presence, I'd love to chat and see how I can help.
+            Let's bring your website dreams to reality together!
+          </p>
         </ScrollAnimation>
 
         <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
           <h3>Here are my main skills:</h3>
         </ScrollAnimation>
         <div className="hard-skills">
-
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.10 * 1000}>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
               <img src={wordpress} alt="Wordpress" />
             </ScrollAnimation>
           </div>
@@ -94,10 +107,10 @@ export function About() {
         </div>
       </div>
       <div className="about-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
-          <img src={VinayakSingh} alt="Vinayak Singh" />
+        <ScrollAnimation animateIn="fadeInRight" delay={0.2 * 1000}>
+          <img src={IllustrationTwo} alt="Vinayak Singh" />
         </ScrollAnimation>
       </div>
     </Container>
-  )
+  );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,39 +1,30 @@
-import { Container } from './styles'
-import { BrowserRouter as Router } from 'react-router-dom'
-import { NavHashLink, HashLink } from 'react-router-hash-link'
-import { useState } from 'react'
+import { Container } from "./styles";
+import { BrowserRouter as Router } from "react-router-dom";
+import { NavHashLink } from "react-router-hash-link";
+import { useState } from "react";
 
-import CV from '../../assets/CV_VinayakSingh.pdf'
+import CV from "../../assets/CV_VinayakSingh.pdf";
+import SunIcon from "../../assets/sun-regular.svg"; // Assuming the path to your SVG
+import MoonIcon from "../../assets/moon-solid.svg"; // Assuming the path to your SVG
+
 export function Header() {
-  const [isActive, setActive] = useState(false)
+  const [isActive, setActive] = useState(false);
+  const [isLightTheme, setIsLightTheme] = useState(true); // Use a boolean state for theme
 
   function toggleTheme() {
-    let html = document.getElementsByTagName('html')[0]
-    html.classList.toggle('light')
+    let html = document.getElementsByTagName("html")[0];
+    html.classList.toggle("light");
+    setIsLightTheme(!isLightTheme); // Toggle theme state
   }
 
   function closeMenu() {
-    setActive(false)
+    setActive(false);
   }
 
   return (
     <Container className="header-fixed">
       <Router>
-        <HashLink smooth to="#home" className="logo">
-          <span>{"<Vinayak "}</span>
-          <span>{" Singh/>"}</span>
-        </HashLink>
-
-        <input
-          onChange={toggleTheme}
-          className="container_toggle"
-          type="checkbox"
-          id="switch"
-          name="mode"
-        />
-        <label htmlFor="switch">Toggle</label>
-
-        <nav className={isActive ? 'active' : ''}>
+        <nav className={isActive ? "active" : ""}>
           <NavHashLink smooth to="#home" onClick={closeMenu}>
             Home
           </NavHashLink>
@@ -51,16 +42,30 @@ export function Header() {
           </a>
         </nav>
 
+        <input
+          onChange={toggleTheme}
+          className="container_toggle"
+          type="checkbox"
+          id="switch"
+          name="mode"
+        />
+        <label htmlFor="switch">Toggle</label>
+        {/* Conditionally render SVG based on theme */}
+        {isLightTheme ? (
+          <img src={SunIcon} alt="Dark Mode" />
+        ) : (
+          <img src={MoonIcon} alt="Light Mode" />
+        )}
         <div
-          aria-expanded={isActive ? 'true' : 'false'}
+          aria-expanded={isActive ? "true" : "false"}
           aria-haspopup="true"
-          aria-label={isActive ? 'Fechar menu' : 'Abrir menu'}
-          className={isActive ? 'menu active' : 'menu'}
+          aria-label={isActive ? "Fechar menu" : "Abrir menu"}
+          className={isActive ? "menu active" : "menu"}
           onClick={() => {
-            setActive(!isActive)
+            setActive(!isActive);
           }}
         ></div>
       </Router>
     </Container>
-  )
+  );
 }
